0e20fd988e4ad4a007ff6f8df100afce600e3755
Mike Manewitz <mike.manewitz@gmail.com>
2011-08-19 19:05:45 -0500

    tweaking selection sort

--- selection_sort.rb
+++ selection_sort.rb
@@ -1,31 +1,37 @@
-# a selection sort finds the minimum value, swaps it with the value in the first position, 
-# and repeats these steps for the remainder of the list.
+#
+#       Mike's Ruby Selection Sort v1.0
+#       Ruby implementation of a selection sort algorithim
+
+# "A selection sort finds the minimum value, swaps it 
+# with the value in the first position, and repeats
+# these steps for the remainder of the list."
 
 require_relative 'create_test_data.rb'
+
+
+class SelectionSort < Object
+
+  def selection_sort(list)
+    sorted = Array.new
+    min_index = list.find_index(list.min)
+
+    list.size.times do
+       min_index = list.find_index(list.min)
+       smallest = list.slice!(min_index)
+     
+       sorted << smallest
+     end
+    return smallest
+  end
+end
 
 list = create_test_data(20, 100)
 
 puts "Unsorted List:"
 p list
-
 puts
-
-class SelectionSort < Object
-  def selection_sort(list)
-    i = 0
-    length = list.size
-    sorted = Array.new
-    length.times do
-      min_index = list.find_index(list.min)
-      smallest = list.slice!(min_index)
-    
-      sorted << smallest
-    end
-    sorted
-end
- 
-end
 
 sort = SelectionSort.new
 sorted = sort.selection_sort(list)
+
 p sorted
