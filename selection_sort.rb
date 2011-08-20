6a13e67204379eac52816f1a9c199e0531e4a4d2
Mike Manewitz <mike.manewitz@gmail.com>
2011-08-19 19:31:58 -0500

    cleanup

--- selection_sort.rb
+++ selection_sort.rb
@@ -1,7 +1,8 @@
 #
 #       Mike's Ruby Selection Sort v1.0
 #       Ruby implementation of a selection sort algorithim
-
+#       8/19/11
+# 
 # "A selection sort finds the minimum value, swaps it 
 # with the value in the first position, and repeats
 # these steps for the remainder of the list."
@@ -14,24 +15,29 @@ class SelectionSort < Object
   def selection_sort(list)
     sorted = Array.new
     min_index = list.find_index(list.min)
-
+   
     list.size.times do
+      # find the index of the smallest value
        min_index = list.find_index(list.min)
+     # remove it by index from the array destructively
        smallest = list.slice!(min_index)
-     
+     # add it to the beginning of a new array
        sorted << smallest
      end
-    return smallest
+    return sorted
   end
 end
 
+# array of 20 values between 0 and 100
 list = create_test_data(20, 100)
 
 puts "Unsorted List:"
 p list
+
 puts
 
 sort = SelectionSort.new
 sorted = sort.selection_sort(list)
 
+puts "Selection Sorted List:"
 p sorted
